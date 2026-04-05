3daaa42598ef6738d1dfda4b99219d1b3af51903
ananyadhagat <146732397+ananyadhagat@users.noreply.github.com>
2026-04-05 14:14:21 +0530

    Time: 6 ms (49.49%), Space: 45.5 MB (84.38%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean judgeCircle(String moves) {
+        int x=0;
+        int y=0;
+        for(int i=0; i<moves.length(); i++){
+            char ch=moves.charAt(i);
+            if(ch=='U'){
+                y++;
+            }
+            else if(ch=='R'){
+                x++;
+            }
+            else if(ch=='L'){
+                x--;
+            }
+            else if(ch=='D'){
+                y--;
+            }
+
+        }
+        if(x==0&&y==0){
+            return true;
+        }
+        else return false;
+    }
+}
