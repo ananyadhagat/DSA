b3d354e5c75c190d9f61ca21dcd35dc39090f60f
ananyadhagat <146732397+ananyadhagat@users.noreply.github.com>
2026-05-12 23:28:06 +0530

    Time: 5 ms (68.7%), Space: 47.3 MB (16.72%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int minDistance(String word1, String word2) {
+        int m =word1.length();
+        int n= word2.length();
+      int[][]dp= new int[m+1][n+1];
+    
+//delete
+//dp[i-1][j]+1 //delete
+//dp[i][j-1]+1//insert in word1 after removing from word 2
+//mdp[i-1][j-1]+1 //replace
+for(int i=0; i<=m; i++){
+    for(int j=0; j<=n; j++){
+if(i==0){dp[i][j]=j;}
+  else  if(j==0){dp[i][j]=i;}
+    
+
+else if(word1.charAt(i-1)==word2.charAt(j-1)){
+dp[i][j]=dp[i-1][j-1];
+}
+else{
+dp[i][j]= 1+Math.min(dp[i-1][j], Math.min(dp[i][j-1], dp[i-1][j-1]));
+    }}
+}
+return dp[m][n];
+    }
+}
